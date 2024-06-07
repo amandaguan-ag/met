bb81445aa35a1540c2b2d45b7a0fe2ec0f163cb2
amandaguan-ag <amandaguan1314@gmail.com>
2024-06-06 19:18:11 -0700

    chore: Style dropdown menu for department selection in DepartmentDropdown component

--- src/components/DepartmentDropdown.jsx
+++ src/components/DepartmentDropdown.jsx
@@ -1,24 +1,46 @@
 import { useState, useEffect } from "react";
 import { fetchDepartments } from "../api/metAPI";
+import "../css/DepartmentDropdown.css";
 
 function DepartmentDropdown({ onChange }) {
   const [departments, setDepartments] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     fetchDepartments().then(setDepartments);
   }, []);
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDepartmentClick = (departmentId) => {
+    onChange(departmentId);
+    setIsOpen(false);
+  };
+
   return (
-    <select onChange={(e) => onChange(e.target.value)} defaultValue="">
-      <option value="" disabled>
-        Select a Department
-      </option>
-      {departments.map((dept) => (
-        <option key={dept.departmentId} value={dept.departmentId}>
-          {dept.displayName}
-        </option>
-      ))}
-    </select>
+    <div className="dropdown toggle">
+      <input
+        id="departmentToggle"
+        type="checkbox"
+        checked={isOpen}
+        onChange={handleToggle}
+      />
+      <label htmlFor="departmentToggle">Select a Department</label>
+      <ul>
+        {departments.map((dept) => (
+          <li key={dept.departmentId}>
+            <a
+              href="#"
+              onClick={() => handleDepartmentClick(dept.departmentId)}
+            >
+              {dept.displayName}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
