1eb7d27497f71dd3dd9b27a60d6273aa355a15b9
amandaguan-ag <amandaguan1314@gmail.com>
2024-06-06 20:08:47 -0700

    chore: Refactor DepartmentDropdown component to use Bootstrap dropdown menu

--- src/components/DepartmentDropdown.jsx
+++ src/components/DepartmentDropdown.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { fetchDepartments } from "../api/metAPI";
-import "../css/DepartmentDropdown.css";
 
 function DepartmentDropdown({ onChange }) {
   const [departments, setDepartments] = useState([]);
@@ -20,18 +19,22 @@ function DepartmentDropdown({ onChange }) {
   };
 
   return (
-    <div className="dropdown toggle">
-      <input
-        id="departmentToggle"
-        type="checkbox"
-        checked={isOpen}
-        onChange={handleToggle}
-      />
-      <label htmlFor="departmentToggle">Select a Department</label>
-      <ul>
+    <div className="dropdown">
+      <button
+        className="btn btn-dark dropdown-toggle"
+        type="button"
+        id="departmentDropdown"
+        data-bs-toggle="dropdown"
+        aria-expanded={isOpen}
+        onClick={handleToggle}
+      >
+        Select a Department
+      </button>
+      <ul className={`dropdown-menu ${isOpen ? "show" : ""}`}>
         {departments.map((dept) => (
           <li key={dept.departmentId}>
             <a
+              className="dropdown-item"
               href="#"
               onClick={() => handleDepartmentClick(dept.departmentId)}
             >
